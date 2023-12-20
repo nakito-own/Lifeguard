06856a85b6c40add047ba5e1dca07df8a63010f0
Nakito <nikitapanov8911@gmail.com>
2023-12-20 04:17:33 +0300

    Normal open drawer button

--- lifeguard/lib/Windows/PersonalAccountWindow.dart
+++ lifeguard/lib/Windows/PersonalAccountWindow.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lifeguard/Windows/MainDrawer.dart';
 
@@ -12,26 +11,24 @@ class PersonalAccount extends StatelessWidget {
           elevation: 8,
           backgroundColor: const Color(0xFF2F2B2B),
           shadowColor: Colors.black.withOpacity(1), // Увеличьте значение альфа-канала
-          title: const Text("Профиль", style: TextStyle(
-            color: Colors.white
-            ),
+          title: const Text(
+            "Профиль",
+            style: TextStyle(color: Colors.white),
           ),
           centerTitle: true,
           automaticallyImplyLeading: false,
-          actions: [
-            Builder(
-              builder: (context) => IconButton(
-                icon: const Icon(
-                  Icons.menu,
-                  size: 30.0, // Увеличьте размер иконки
-                  color: Colors.white, // Установите белый цвет
-                ),
-                onPressed: () {
-                  Scaffold.of(context).openDrawer(); // Открывает боковое меню
-                },
+          leading: Builder(
+            builder: (context) => IconButton(
+              icon: const Icon(
+                Icons.menu,
+                size: 30.0,
+                color: Colors.white,
               ),
+              onPressed: () {
+                Scaffold.of(context).openDrawer(); // Открывает боковое меню
+              },
             ),
-          ],
+          ),
         ),
         drawer: MainDrawer(),
         body: Container(
